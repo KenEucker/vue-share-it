f137f0ef344372916122e527363a39e7295ff6e3
Ken Eucker <keneucker@gmail.com>
2023-12-12 10:08:56 -0800

    Update index.js
    Removes something and changes "Vue" to "app".

--- index.js
+++ index.js
@@ -6,17 +6,12 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 const shareItPlugin =  {
-  install (Vue, options) {
+  install (app, options) {
     library.add(faTwitter, faFacebookF, faWhatsapp, faLinkedin, faRedditAlien, faEnvelope);
-    Vue.component('font-awesome-icon', FontAwesomeIcon);
-    Vue.component('share-it', shareIt);
-    Vue.component('share-button', shareButton);
+    app.component('font-awesome-icon', FontAwesomeIcon);
+    app.component('share-it', shareIt);
+    app.component('share-button', shareButton);
   }
 };
 
 export default shareItPlugin;
-
-// Automatic installation if Vue has been added to the global scope.
-if (typeof window !== 'undefined' && window.Vue) {
-  window.Vue.use(shareItPlugin)
-}
